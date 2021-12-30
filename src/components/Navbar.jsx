cf544ebf25ef1af60706344ade495849e4048f23
Stefano Acuña <ganriel.acuna.reg27@gmail.com>
2021-12-29 19:42:48 -0500

    cambios en Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,19 @@
 import { FcSettings, FcHome, FcRight } from 'react-icons/fc'
+import { CgProfile } from 'react-icons/cg'
 import { ImProfile } from 'react-icons/im'
 import { Link } from 'react-router-dom'
-import {  useDispatch , useSelector} from 'react-redux'
-import { logOut} from '../store/user'
+import { useDispatch, useSelector } from 'react-redux'
+import { logOut } from '../store/user'
+import { useState } from 'react'
 
 let NavBar = (props) => {
 
     let distpach = useDispatch();
     let user = useSelector(state => state.user.user.userInfo)
-    
-    let doLogOut = ()=>{
+    let [today] = useState(new Date())
+    const [isActive, setisActive] = useState(false);
+
+    let doLogOut = () => {
         distpach(
             logOut()
         )
@@ -23,35 +27,53 @@ let NavBar = (props) => {
                     </span>
                     Inicio
                 </Link>
+                <p className="nav-item mt-4 is-size-7 mx-4">
+                        <span className='has-text-weight-medium'>Fecha: </span>
+                        {today.getDay()}-{today.getMonth() < 10 ? `0${today.getMonth()}` : today.getMonth()}-{today.getFullYear()}
+                    </p>
+                <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false"
+                    data-target="navbarBasicExample" onClick={() => {
+                        setisActive(!isActive);
+                    }}>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
             </div>
 
 
 
-            <div className="navbar-item has-dropdown is-hoverable">
-                <span className="navbar-link has-text-weight-bold">
-                    Hola, { user.nombre} { user.apellido}
-                </span>
-                <div className="navbar-dropdown is-boxed">
-                    <Link className="navbar-item" to="/change-password" >
-                        <span className="mr-2">
-                            <FcSettings />
-                        </span>
-                        Cambiar contraseña
-                    </Link>
-                    <Link className="navbar-item" to="/cv">
-                        <span className="mr-2">
-                            <ImProfile/>
-                        </span>
-                        CV</Link>
-                    <a className="navbar-item" onClick={ ()=>doLogOut()}>
-                        <span className="mr-2"> <FcRight /></span>
-                        Cerrar sesión
-                    </a>
+            <div className={`navbar-menu ${isActive ? "is-active" : ""}`} id="navbarBasicExample">
+                
+                <div className="navbar-end">
 
+                    <div className="navbar-item has-dropdown is-hoverable is-pulled-rig">
+                        <span className="navbar-link has-text-weight-bold">
+                        <CgProfile/> Hola, {user.nombre} {user.apellido}
+                        </span>
+                        <div className="navbar-dropdown is-boxed">
+                            <Link className="navbar-item" to="/change-password" >
+                                <span className="mr-2">
+                                    <FcSettings />
+                                </span>
+                                Cambiar contraseña
+                            </Link>
+                            <Link className="navbar-item" to="/cv">
+                                <span className="mr-2">
+                                    <ImProfile />
+                                </span>
+                                CV</Link>
+                            <a className="navbar-item" onClick={() => doLogOut()}>
+                                <span className="mr-2"> <FcRight /></span>
+                                Cerrar sesión
+                            </a>
+
+                        </div>
+
+
+
+                    </div>
                 </div>
-
-
-
             </div>
 
 
